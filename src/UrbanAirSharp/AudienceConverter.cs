2671b4cb6b95852eacaf3527802b3110e9740e25
Thomas Kenvin <tptk@compsoft.com>
2019-09-18 11:26:12 +0100

    Adds Audience and DeviceType converters.

---
+++ src/UrbanAirSharp/AudienceConverter.cs
@@ -0,0 +1,40 @@
+﻿using Newtonsoft.Json;
+using UrbanAirSharp.Dto;
+
+namespace UrbanAirSharp
+{
+    public class AudienceConverter : JsonConverter<Audience>
+    {
+        public override Audience ReadJson(JsonReader reader, System.Type objectType, Audience existingValue, bool hasExistingValue, JsonSerializer serializer)
+        {
+            if (JsonToken.String.Equals(reader.TokenType) &&
+                "all".Equals(reader.Value?.ToString()))
+            {
+                return Audience.All;
+            }
+            var serialiserSettings = new JsonSerializerSettings
+            {
+                NullValueHandling = serializer.NullValueHandling
+            };
+            var defaultSerialiser = JsonSerializer.CreateDefault(serialiserSettings);
+            return defaultSerialiser.Deserialize(reader, objectType) as Audience;
+        }
+
+        public override void WriteJson(JsonWriter writer, Audience value, JsonSerializer serializer)
+        {
+            if (value.IsAll())
+            {
+                serializer.Serialize(writer, "all");
+            }
+            else
+            {
+                var serialiserSettings = new JsonSerializerSettings
+                {
+                    NullValueHandling = serializer.NullValueHandling
+                };
+                var defaultSerialiser = JsonSerializer.CreateDefault(serialiserSettings);
+                defaultSerialiser.Serialize(writer, value);
+            }
+        }
+    }
+}
---
+++ src/UrbanAirSharp/DeviceTypeConverter.cs
@@ -0,0 +1,30 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using UrbanAirSharp.Type;
+
+namespace UrbanAirSharp
+{
+    public class DeviceTypeConverter : JsonConverter<IList<DeviceType>>
+    {
+        public override IList<DeviceType> ReadJson(JsonReader reader, System.Type objectType, IList<DeviceType> existingValue, bool hasExistingValue, JsonSerializer serializer)
+        {
+            if (JsonToken.String.Equals(reader.TokenType) &&
+                "all".Equals(reader.Value?.ToString()))
+            {
+                return null;
+            }
+            var serialiserSettings = new JsonSerializerSettings
+            {
+                NullValueHandling = serializer.NullValueHandling
+            };
+            var defaultSerialiser = JsonSerializer.CreateDefault(serialiserSettings);
+            return defaultSerialiser.Deserialize(reader, objectType) as IList<DeviceType>;
+        }
+
+        public override void WriteJson(JsonWriter writer, IList<DeviceType> value, JsonSerializer serializer)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
