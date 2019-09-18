6de4bf2996975b35307765ca97b121dc9bc851c4
Thomas Kenvin <tptk@compsoft.com>
2019-09-18 11:26:50 +0100

    Slightly improved and expanded unit tests.

--- src/UnitTests/AudienceTests.cs
+++ src/UnitTests/AudienceTests.cs
@@ -1,4 +1,5 @@
 ﻿using Newtonsoft.Json;
+using UrbanAirSharp;
 using UrbanAirSharp.Dto;
 using Xunit;
 
@@ -9,9 +10,9 @@ namespace UnitTests
         [Fact]
         public void DeserialisationTest1()
         {
-            var contentJson = "{ \"named_user\": \"1008009\" }";
-            var actual = JsonConvert.DeserializeObject<Audience>(contentJson);
-            Assert.Equal("1008009", actual.NamedUser);
+            var contentJson = "{\"named_user\":\"Bob\"}";
+            var actual = JsonConvert.DeserializeObject<Audience>(contentJson, new AudienceConverter());
+            Assert.Equal("Bob", actual.NamedUser);
             Assert.Null(actual.IosDeviceId);
             Assert.Null(actual.Not);
             Assert.False(actual.IsAll());
@@ -21,7 +22,7 @@ namespace UnitTests
         public void DeserialisationTest2()
         {
             var contentJson = "\"all\"";
-            var actual = JsonConvert.DeserializeObject<Audience>(contentJson);
+            var actual = JsonConvert.DeserializeObject<Audience>(contentJson, new AudienceConverter());
             Assert.Null(actual.IosDeviceId);
             Assert.Null(actual.Not);
             Assert.True(actual.IsAll());
@@ -31,8 +32,16 @@ namespace UnitTests
         public void SerialisationTest1()
         {
             var audience = Audience.All;
-            var contentJson = JsonConvert.SerializeObject(audience);
+            var contentJson = JsonConvert.SerializeObject(audience, new AudienceConverter());
+            Assert.Equal("\"all\"", contentJson);
+        }
 
+        [Fact]
+        public void SerialisationTest2()
+        {
+            var audience = new Audience(UrbanAirSharp.Type.AudienceType.NamedUser, "Bob");
+            var contentJson = JsonConvert.SerializeObject(audience, new JsonSerializerSettings() { NullValueHandling = NullValueHandling.Ignore, Converters = new[] { new AudienceConverter() } });
+            Assert.Equal("{\"named_user\":\"Bob\"}", contentJson);
         }
     }
 }
